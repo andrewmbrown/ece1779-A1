b2ae5fd39c70e34a754e70a3f2e605de57e81614
Ash T Kumar <ta.kumar97u@gmail.com>
2021-10-18 17:00:55 -0400

    basic database stuff

--- app/config.py
+++ app/config.py
@@ -1,4 +1,9 @@
 import os 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+IMG_LOCATION = '../../dbimages/'
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'test_key'
+    # SQLALCHEMY_DATABASE_URI = 'mysql://root:ece1779pass@localhost/appdb'
+    IMG_FOLDER = IMG_LOCATION
--- app/models.py
+++ app/models.py
@@ -1,1 +1,17 @@
-# Nothing yet
+from app import db 
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(32), index=True, unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+    salt = db.Column(db.Integer, nullable=False)
+    gallery = db.relationship('ImageLocation', backref='uploader', lazy='dynamic')
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+class ImageLocation(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    location = db.Column(db.String(1024), index=True, unique=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    def __repr__(self):
+        return '<Image Path: {}>'.format(self.location)
