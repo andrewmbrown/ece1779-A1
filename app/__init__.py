b61ee943ebc047a5ba7cc3a1ce81a878146e2409
Ash T Kumar <ta.kumar97u@gmail.com>
2021-10-18 14:41:32 -0400

    adding some changes

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,7 @@
 from flask import Flask
+from config import Config 
+
 app = Flask(__name__)  # create app of instance Flask
+app.config.from_object(Config)
+
 from app import routes
--- app/forms.py
+++ app/forms.py
@@ -1,1 +1,10 @@
-# nothing yet
+from flask_wtf import FlaskForm 
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+
+class LoginForm(FlaskForm):
+    user = StringField('user', validators=[DataRequired()])
+    pwd = PasswordField('pass', validators[DataRequired()])
+    remember = BooleanField('remember me')
+    submit = SubmitField('login')
+
