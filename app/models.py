50ebee06c0afbccf5320f0e5b0e3124d73637785
andrewmbrown <17amb@queensu.ca>
2021-10-19 01:33:24 -0400

    added login functionality

--- app/models.py
+++ app/models.py
@@ -1,13 +1,33 @@
 from app import db 
+# hashing passwords
+from werkzeug.security import check_password_hash, generate_password_hash
+# authenticating users, checking active status, anonyimity, id
+from flask_login import UserMixin
+from app import login
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(32), index=True, unique=True, nullable=False)
+    username = db.Column(db.String(64), index=True, unique=True)
     password_hash = db.Column(db.String(128), nullable=False)
     salt = db.Column(db.Integer, nullable=False)
     gallery = db.relationship('ImageLocation', backref='uploader', lazy='dynamic')
     def __repr__(self):
         return '<User {}>'.format(self.username)
+    
+    # Class now has method to do secure password verification
+    # also to check password (hash again and check if they match)
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+    
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
 
 class ImageLocation(db.Model):
     id = db.Column(db.Integer, primary_key=True)
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,10 @@
-from flask import render_template, flash, redirect, url_for
-from app import app
-from app.forms import LoginForm
+from flask import render_template, flash, redirect, url_for, request
+from flask_migrate import current
+from app import app, db
+from app.forms import LoginForm, RegistrationForm
+from werkzeug.urls import url_parse
+from flask_login import current_user, login_user, logout_user, login_required
+from app.models import User
 
 '''
 # NOTE: This is all placeholder code, it will be changed
@@ -9,23 +13,9 @@ from app.forms import LoginForm
 
 # fix this later
 @app.route('/')
-def starter():
-    return redirect(url_for('login'))
-
-# @app.route('/')
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        flash('login for user {}, remember={}'. format(
-            form.user.data, form.remember.data
-        ))
-        return redirect(url_for('index'))
-    return render_template('login.html', title='Login', form=form)
-
 @app.route('/index')  # use as register fxn as callbacks for certain events
+@login_required
 def index():
-    user = {'username': 'Andrew'}
     # render_template() invokes jinja2 substituting {{...}} blocks with corresponding values
     posts = [
         {
@@ -38,3 +28,43 @@ def index():
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+
+# @app.route('/')
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
+    return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
