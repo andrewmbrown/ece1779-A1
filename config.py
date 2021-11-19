68b1371a34fd8fce7103e99facc2c2988054defd
Ash T Kumar <ta.kumar97u@gmail.com>
2021-11-19 12:32:48 -0500

    rds db attempt

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ IMG_LOCATION = '../../dbimages/'
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
+    #SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    #    'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:ece1779pass@ece1779-db-1.cnsxozjecorh.us-east-1.rds.amazonaws.com:3306/ece1779-db-1"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
